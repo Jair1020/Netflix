189258dfb86f09a751cd3650df5ada85b94de4c4
Jair Avila <jair_p1020@hotmail.com>
2022-06-04 22:32:27 -0500

    front

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <Routes>
       <Route path='/' element={<Home/>} />
-
+      </Routes>
     </Router>
   );
 }
